aa354573614434dbd6188b2161842d3d3af46ff7
Cole Rossman <colerossman99@gmail.com>
2022-01-31 18:16:59 -0800

    renderIngredients function and test finished and work. tests.js file created for pathing so Qunit test would run

--- test/utils.tests.js
+++ test/utils.tests.js
@@ -1,1 +1,10 @@
-import './example.test.js';
+import { renderIngredients } from '../utils.js';
+const test = QUnit.test;
+
+test('renderIngredients should return an <li></li>', (expect) => { 
+    const expected = `<li>15 Grams of Salt</li>`;
+    
+    const actual = renderIngredients({ quantity: 15, measurement: 'Grams', ingredient: 'Salt' });
+
+    expect.equal(actual.outerHTML, expected);
+});
