87ff25571ee18fa100914017636ead1724fc6089
Cole Rossman <colerossman99@gmail.com>
2022-01-31 17:29:36 -0800

    dom elements grabbed in app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // import functions and grab DOM elements
+const form = document.getElementById('add-ingredient');
+const ingredientList = document.getElementById('ingredient-ul');
+const mealsList = document.getElementById('meals-ul');
 
+const removeButton = document.getElementById('remove');
+const saveButton = document.getElementById('save-meal');
 // let state
 
 // set event listeners 
